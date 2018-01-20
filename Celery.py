fbbb50bed7c2cd5cad339e5a985d9303f265f4d9
Firexd2 <firedenis@yandex.ru>
2018-01-20 21:24:59 +0300

    Fix Celery-2

--- Celery.py
+++ Celery.py
@@ -1,6 +1,5 @@
 import os
 from celery import Celery
-from ProjectOnlineMarket import settings
 from ProjectOnlineMarket.settings import BROKER_URL
 
 celery = Celery('tasks', broker=BROKER_URL)
@@ -8,4 +7,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectOnlineMarket.settings')
 app = Celery('ProjectOnlineMarket')
 app.config_from_object('django.conf:settings')
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
