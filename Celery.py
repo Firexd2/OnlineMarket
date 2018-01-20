3019d0181ab56f29f6234298afa824fc127d8459
Firexd2 <firedenis@yandex.ru>
2018-01-20 22:35:02 +0300

    Celery

--- Celery.py
+++ Celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from ProjectOnlineMarket import settings
 from ProjectOnlineMarket.settings import BROKER_URL
 
 celery = Celery('tasks', broker=BROKER_URL)
@@ -7,4 +8,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectOnlineMarket.settings')
 app = Celery('ProjectOnlineMarket')
 app.config_from_object('django.conf:settings')
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
